55554e905b111dcd12673d0e9ceeae302fa5fc7a
Orkun Alkan <44678771+Pikrodafni@users.noreply.github.com>
2020-12-04 22:52:28 +0300

    second commit

--- main.py
+++ main.py
@@ -1,8 +1,45 @@
-try:
-   thread.start_new_thread( print_time, ("Thread-1", 2, ) )
-   thread.start_new_thread( print_time, ("Thread-2", 4, ) )
-except:
-   print "Error: unable to start thread"
+import random
+import threading
+import time
 
-while 1:
-   pass
+exitFlag = 0
+
+class myThread (threading.Thread):
+   def __init__(self, name, mode, floor, destination ,direction ,capacity ,count_inside ,inside ,active):
+      threading.Thread.__init__(self)
+      self.name = name
+      self.mode = mode
+      self.floor = floor
+      self.destination = destination
+      self.direction = direction
+      self.capacity = capacity
+      self.count_inside = count_inside
+      self.inside = inside
+      self.active = active
+
+   def run(self):
+      print("Starting " + self.name)
+      print_time(self.name, 5, 0.5)
+      print("Exiting " + self.name)
+
+def print_time(threadName, counter, delay):
+   while counter:
+      if exitFlag:
+         threadName.exit()
+      time.sleep(delay)
+      print("%s: %s" % (threadName, time.ctime(time.time())))
+      counter -= 1
+
+
+def girenSayisi():
+   insan = random.randint(1, 10)
+   return insan
+
+def hedefKat():
+   kat = random.randint(1,4)
+   return kat
+
+
+thread = myThread("asansör1","idle",0,0,"up",10,girenSayisi(),0,True)
+
+thread.start()
